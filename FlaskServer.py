39490549ef9da750b0b6828918dbb4921769e73f
Narfis <38036515+Narfis@users.noreply.github.com>
2022-10-18 12:18:58 +0200

    More text returned to the website

--- FlaskServer.py
+++ FlaskServer.py
@@ -1,5 +1,6 @@
 import URL_Object as urlObject
 import UrlController as urlCtr
+import PortCheck
 from flask import Flask, request
 import webscraper
 
@@ -16,25 +17,36 @@ def members():
         return {"valid": str(attributeDict["valid"]), "report": report}
 
     report = ""
-    for i in range(1,3):
+    for i in range(1,5):
         if i == 1:
-            report += (f"- Website has {'a' if attributeDict[1] == 1 else 'no'} favicon ")
+            report += (f"- Website has {'a' if attributeDict[i] == 1 else 'no'} favicon ")
         if i == 2:
-            report += (f"\n- Website uses {attributeDict[2].upper()} protocol")
+            report += (f"\n- Website uses {attributeDict[i].upper()} protocol")
+        if i == 3:
+            report += (f"\n- Website is running on the {'right' if attributeDict[i] == True else 'wrong'}  port")
+        if i == 4:
+            report += (f"\n- The age of the webiste is {attributeDict[i]}")
     return {"valid": str(attributeDict["valid"]), "report": report}
+
 
 def main(url):
     ctr = urlCtr.UrlController()
-    valid = ctr.validateUrl(url)
-    if not valid:
+    ip = ctr.validateUrl(url)
+    if not ip:
         return {'valid': False}
     #Continue
     scraper = webscraper.Webscraper(url)
+    portCheck = PortCheck.PortCheck()
+    
+
+
 
     favIcon = scraper.isExistFavicon()
     protocol = scraper.exfiltrateProtocol()
+    age = scraper.exfiltrateSiteAge()
+    port = portCheck.checkPorts(ip, protocol)
 
-    return {'valid' : True, 1 : favIcon, 2 : protocol}
+    return {'valid' : True, 1 : favIcon, 2 : protocol, 3 : port, 4 : age}
 
 
 
