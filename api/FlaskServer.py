0b8ab68aadfbd6d2dbbd7987964a6ba73fed06a9
Loadeumn <hugonordenstrom@hotmail.com>
2022-10-31 16:27:40 +0100

    added cors for permisions to backend from frontend port

--- api/FlaskServer.py
+++ api/FlaskServer.py
@@ -4,10 +4,11 @@ import PortCheck
 from flask import Flask, request
 import webscraper
 import time
-
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/backendAPI')
