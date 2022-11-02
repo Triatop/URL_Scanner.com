108080e771f00f8cbc295928763a98dd57797918
Narfis <Sebban.hansson@gmail.com>
2022-11-02 17:48:26 +0100

    Changed the variable "attributes" to "weights"

--- api/SafeEvaluator.py
+++ api/SafeEvaluator.py
@@ -1,10 +1,10 @@
 class SafeEvaluator:
 
     def __init__(self):
-        self.attributes = (5, 2, 4)
+        self.weights = (5, 2, 4)
         self.th = 3
 
     def isSafe(self, dict):
-        zippedLists = list(zip(self.attributes, (dict.values())))
+        zippedLists = list(zip(self.weights, (dict.values())))
 
         return (sum([val1 * val2 for val1, val2 in zippedLists]) - self.th) > 0
