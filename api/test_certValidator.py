083674cb75ade181df927bf6be2119bcfe744539
Triatop <kimdenlille@gmail.com>
2022-11-09 12:51:26 +0000

    Update test_certValidator.py

--- api/test_certValidator.py
+++ api/test_certValidator.py
@@ -3,12 +3,12 @@ import certValidator
 def test_SelfSigned():
     url = "https://cdn.redhat.com"
     cv = certValidator.CertValidator(url)
-    assert (cv.processData() == 1)
+    assert (cv.processData() == 0)
 def test_hostnameMismatch():
     url = "https://ev-www.redhat.com.edgekey.net"
     cv = certValidator.CertValidator(url)
-    assert (cv.processData() == 1)
+    assert (cv.processData() == 0)
 def test_activeCert():
     url = "https://www.google.com"
     cv = certValidator.CertValidator(url)
-    assert (cv.processData() == 0)
+    assert (cv.processData() == 1)
