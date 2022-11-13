ea78cc38adc8bc94fdd801e59b027ebcab5ea917
MrMoneyVV <113528038+MrMoneyVV@users.noreply.github.com>
2022-11-13 14:45:49 +0100

    Added Duplication check + Fixed massive error + small formatting changes
    I might have forgotten to change the lookup from a proof of concept style check (it was not in the loop and used the same constant for its checks) which led it to only looking at the first link provided and looked at it many times...

--- api/MaliciousLinks.py
+++ api/MaliciousLinks.py
@@ -6,9 +6,9 @@ class MaliciousLinks:
     def __init__(self):
         self.m_links = []
         self.m_url = ''
-        self.m_apiKey = '' #Replace with non-personal ID 
+        self.m_apiKey = 'K5FSMq7EFoOqwjx1B0ED2P5TMkPp1bcy' #Replace with non-personal ID 
 
-    def getData(self, i_links, i_url):
+    def getData(self, i_url, i_links):
         self.m_links = i_links
         self.m_url = list(filter(None, i_url.split('/')))[1]      #get top level domains of the list trim off protocol and anything after
         return 1
@@ -31,22 +31,39 @@ class MaliciousLinks:
         #print(self.m_links)
         return 1
 
+    def removeDup(self):
+        f_it = len(self.m_links)
+        i = 0
+        while i < f_it:
+            j = i + 1
+            icompare = list(filter(None, self.m_links[i].split('/')))[1]
+            while j < f_it:                                                     #Nestled while loop :O
+                jcompare = list(filter(None, self.m_links[j].split('/')))[1]
+                if icompare == jcompare:
+                    self.m_links.pop(j)
+                    f_it -= 1
+                    j -= 1
+                j += 1
+            i += 1
+        return 1
+
     def maliciousCheck(self):      #Send an array of links to Virustotal and get the response
         self.externalCheck()
+        self.removeDup()
         l_response = []
-        v_url = 'https://www.ipqualityscore.com/api/json/url/%s/%s' % (self.m_apiKey, urllib.parse.quote_plus(self.m_links[0]))  #virustotal url  parser.add_argument("-u", "--url-list", help="bulk url analysis")
         
         for i in range(len(self.m_links)):
             try:
+                v_url = 'https://www.ipqualityscore.com/api/json/url/%s/%s' % (self.m_apiKey, urllib.parse.quote_plus(self.m_links[i]))  #IPQS url
                 m_response = requests.get(v_url)
-                #print(m_response.text)
+                print(m_response.text)
                 l_response.append(json.loads(m_response.text))
             except ValueError as e:
                 print("Rate Limit detected:", e)
         return l_response
 
-    def isExternalMalicious(self):  #Parse the value from IPQS 
-        m_extLinks = 0
+    def isExternalSafe(self):  #Parse the value from IPQS 
+        m_extLinks = 1
         m_sum = 0
         m_lrgi = 0
         m_resp = self.maliciousCheck()
@@ -60,5 +77,5 @@ class MaliciousLinks:
         #print(r_score)
 
         if m_sum > 100 or r_score[m_lrgi] > 75:             # IPQS USES	Overall threat score from: 0 (clean) to 100 (high risk)
-            m_extLinks = 1                                  # Do we want binary or severity?
+            m_extLinks = 0                                  # Do we want binary or severity?
         return m_extLinks
