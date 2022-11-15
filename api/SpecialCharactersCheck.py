6f108cef0d91ea16e61e9871551083e2eebfbcb0
Elliot Marcusson <elmr20@student.bth.se>
2022-11-15 08:44:57 +0100

    Update SCCheck

--- api/SpecialCharactersCheck.py
+++ api/SpecialCharactersCheck.py
@@ -1,37 +1,30 @@
 from urllib.parse import urlparse
 
+
 class SCCheck:
     def __init__(self):
         self.l_URL = ''
-        self.value = 0                      # Amount of bad special charachters
+        self.sev = 0
 
-    def setData(self, i_URL):               # Get the URL
+    def setData(self, i_URL):
         self.l_URL = i_URL
         return 1
 
-    def checkAtSign(self):                  # Url having "@" symbol -> Phising
-        sign = '@'
-        for c in self.l_URL:
-            if c == sign:
-                self.value += 1
-                return True
-        return False
-
-    def checkHyphen(self):                  # Domain name part includes "-" -> Phishing
+    def processData(self):                  
         sign = '-'
         domain = urlparse(self.l_URL).netloc
         for c in domain:
             if c == sign:
-                self.value += 1
-                return True
-        return False
-
-    def checkDoubleForwardSlash(self):      # The position of the last occurrence of "//" in the URL > 7 -> Phising
+                self.sev = 1
+        subdomain = urlparse(self.l_URL).hostname
+        if subdomain.count('.')+1 > 2:
+            self.sev = 2
+        sign = '@'
+        for c in self.l_URL:
+            if c == sign:
+                self.sev = 3
         sign = '/'
         for i in range(7, len(self.l_URL)-1):
             if self.l_URL[i] == self.l_URL[i+1]:
                 if self.l_URL[i] == sign:
-                    self.value += 1
-                    return True
-        return False
-
+                    self.sev = 4
