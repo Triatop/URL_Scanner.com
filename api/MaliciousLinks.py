cf5dd4974cb0b2f9ca55e81abd3a006fc0075afc
MrMoneyVV <113528038+MrMoneyVV@users.noreply.github.com>
2022-11-19 12:29:00 +0100

    Added edge case handling and ability to track amount of malicious links
    "/"links and "//" domains have been handled. A variable to keep track of the amount of suspicious links.
    Added testing to reflect these changes.

--- api/MaliciousLinks.py
+++ api/MaliciousLinks.py
@@ -8,7 +8,11 @@ class MaliciousLinks:
         self.m_links = []
         self.m_url = ''
         self.m_apiKey = '' #Replace with non-personal ID 
+        self.m_NrOfMalLinks = 0
 
+    def getNrOfMalLinks(self):
+        return self.m_NrOfMalLinks
+        
     def getData(self, i_url, i_links):
         self.m_links = i_links
         self.m_url = list(filter(None, i_url.split('/')))[1]      #get top level domains of the list trim off protocol and anything after
@@ -19,12 +23,20 @@ class MaliciousLinks:
         i = 0
         while(i < f_iterations):
             try:
-                compare = list(filter(None, self.m_links[i].split('/')))[1] #Divide and take TLD Example "www.google.com"
-                if self.m_url == compare:                                   #If it is the same level domain as the website our tool is checking.
+                if self.m_links[i][0] == '/' and self.m_links[i][1] != '/': #/link är en intern länk
                     self.m_links.pop(i)
                     i -= 1
-                    f_iterations -= 1                                       
-            except:                                              #Has no "/" or is not a link Example "garykessler@gmail.com"
+                    f_iterations -= 1  
+                else:
+                    if self.m_links[i][0] == '/' and self.m_links[i][1] == '/': #// is URL with a relative (unspecified) protocol
+                        self.m_links[i][0].pop()                                #Drrop the // and check duplicate
+                        self.m_links[i][1].pop()
+                    compare = list(filter(None, self.m_links[i].split('/')))[1] #Divide and take TLD Example "www.google.com"
+                    if self.m_url == compare:                                   #If it is the same level domain as the website our tool is checking.
+                        self.m_links.pop(i)
+                        i -= 1
+                        f_iterations -= 1                                       
+            except:                                                         #Has no "/" or is not a link Example "garykessler@gmail.com"
                 self.m_links.pop(i)                                         #Remove the offending non-link
                 i -= 1                                                      #step back one to not mess up i+1
                 f_iterations -= 1                                           #Adjust iterations to match new list size
@@ -77,9 +89,12 @@ class MaliciousLinks:
         for i in range(len(m_resp)):
             r_score.append(int(m_resp[i]['risk_score']))    #"risk_score":0,
             m_sum += r_score[i]                             #keep a sum of how bad the links are
+            if r_score[i] > 54:                             #55 is considered suspicious
+                self.m_NrOfMalLinks += 1
             if r_score[m_lrgi] < r_score[i] :
                 m_lrgi = i                                  #keep ahold of the worst link
         #print(m_sum, m_lrgi)
+        #print(self.m_links[m_lrgi])
         #print(r_score)
 
         if m_sum > 0:                                       #IPQS USES	Overall threat score from: 0 (clean) to 100 (high risk)
--- api/test_MaliciousLinks.py
+++ api/test_MaliciousLinks.py
@@ -1,5 +1,12 @@
 from MaliciousLinks import MaliciousLinks
 
+def test_getNrOfMalLinks():
+    m_al = MaliciousLinks()
+    assert(m_al.getNrOfMalLinks() == 0)
+    m_al.m_NrOfMalLinks = 3
+    assert(m_al.getNrOfMalLinks() == 3)
+    return
+
 def test_getData():
     m_al = MaliciousLinks()
     url1 = 'https://thepiratebay.org/'
@@ -12,7 +19,9 @@ def test_getData():
 def test_externalCheck():
     m_al = MaliciousLinks()
     url1 = 'https://www.youtube.com/'
-    urls = ['https://mail.google.com/mail/u/0/#inbox', 'https://www.youtube.com/feed/subscriptions', 'https://www.youtube.com/feed/history', 'https://www.google.com/', 'https://github.com/']
+    urls = ['https://mail.google.com/mail/u/0/#inbox', 'https://www.youtube.com/feed/subscriptions', 'https://www.youtube.com/feed/history', 
+            'https://www.google.com/', 'https://github.com/', '/info', '//www.youtube.com'] #/is a relative link(internal) //is a link with unspecified protocol
+
     comp = ['https://mail.google.com/mail/u/0/#inbox', 'https://www.google.com/', 'https://github.com/']
     m_al.getData(url1, urls)
     assert(m_al.externalCheck() == 1)
@@ -34,7 +43,7 @@ def test_maliciousCheck():
     url1 = 'https://twitter.com/'
     urls = ['https://www.youtube.com/', 'https://www.google.com/']
     m_al.getData(url1, urls)
-    assert(m_al.maliciousCheck()) #very hard to test, returns a list of very large dictionaries... impossible to simulate due to one of the objects return being current time.
+    assert(m_al.maliciousCheck()) #very hard to test, returns a list of very large dictionaries... impossible to simulate due to one of the objects returned being current time.
     return
 
 def test_isExternalSafe():
@@ -42,7 +51,7 @@ def test_isExternalSafe():
     m_al2 = MaliciousLinks()
     url1 = 'https://twitter.com/'
     mal_urls = ['https://italarizege.xyz/redirect?tid=855295', 'https://festivalesperu.site/']
-    saf_urls = ['https://www.youtube.com/', 'https://www.google.com/']
+    saf_urls = ['https://www.youtube.com/', 'https://www.google.com/', '//www.google.com/', '//facebook.com']
     m_al1.getData(url1, mal_urls)
     m_al2.getData(url1, saf_urls)
     assert(m_al1.isExternalSafe() == 0)
