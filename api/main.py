e2cd4e2747515a8af6058114b1173058f13e8d1a
Loadeumn <hugonordenstrom@hotmail.com>
2022-11-22 18:07:49 +0100

    added port check object in main

--- api/main.py
+++ api/main.py
@@ -10,6 +10,7 @@ from SafeEvaluator import SafeEvaluator
 from ReportMaker import ReportMaker
 from SpecialCharactersCheck import SpecialCharactersCheck
 from certValidator import CertValidator
+from PortCheck import PortCheck
 
 
 def main(url1, report=True):
@@ -23,10 +24,12 @@ def main(url1, report=True):
         urlRedirect = True
     else:
         urlRedirect = False
+
+    ip = u_ctrl.validateUrl(url1)
     
 
     #Check if wevsite is valid (exists) - else early return
-    if not u_ctrl.validateUrl(url1): #<--- if website fails to validate
+    if not ip: #<--- if website fails to validate
         return {"valid": "False", "report": "- Invalid URL, website does not exist - check for spelling errors"}
 
     #Attribute Classes
@@ -38,6 +41,7 @@ def main(url1, report=True):
     u_cert = CertValidator()
     u_len = LengthURL()
     u_age = SiteAge_URL() 
+    u_port = PortCheck()
 
     #Funciton Classes
     w_scrap = Webscraper()
@@ -49,6 +53,8 @@ def main(url1, report=True):
     u_scc.setData(url1)
     w_scrap.setURL(url1)
     u_mlin.getData(url1, w_scrap.findLinks())
+    u_obj.setIP(ip)                                     #Try Set IP We don't use it for anything though
+
 
     u_len.getData(url1) #URL Size check
     u_prot.getData(w_scrap.exfiltrateProtocol()) #GETTING PROTOCOLS
@@ -58,9 +64,8 @@ def main(url1, report=True):
     u_obj.setURLLinks(u_mlin.isExternalSafe())                          #Are the external links malicious/How malicious are they?
     u_obj.setURLFavIcon(u_fav.hasFavicon(w_scrap.extractFavicon()))     #URL Fav Icon check
     u_obj.setURLSecureProtocol(u_prot.isSecure())                       #Security check
-    u_obj.setCheckPort(w_scrap.exfiltrateProtocol())                    #Is the site runnig on the right port?
+    u_obj.setCheckPort(u_port.checkPorts(u_obj.getIP(), w_scrap.exfiltrateProtocol()))                    #Is the site runnig on the right port?
     u_obj.setURLLength(u_len.isURLLong())                               #Is it too long
-    u_obj.setIP(u_ctrl.getIP(url1))                                     #Try Set IP We don't use it for anything though
     u_obj.setURLSiteAge(u_age.isInLimit(w_scrap.exfiltrateSiteAge()))   #How old is site? 
     u_obj.setSpecialCharater(u_scc.processData())                       #Looking for special charactes
     u_obj.setCertificateValid(u_cert.processData(url1))                 #Certificate validation
@@ -70,6 +75,8 @@ def main(url1, report=True):
     u_obj.makeDict()                                    
     r_mkr.createReport(u_obj.getDict(), w_scrap.exfiltrateSiteAge().days)
     u_obj.setSafe(u_safe.isSafe(u_obj.getDict()))                 #Safe eveluator check
+
+    print(u_obj.getDict())
 
     if(report):
         return {"valid": "True","report": r_mkr.getReport(), "binarySafe": u_obj.getSafe(), "reDirect": f"\n\nRedirected: {urlRedirect} \nScanning: {url1}"}
