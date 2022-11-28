c6b81178074648a1788146f44791aa6f9f8ef2ff
Elliot Marcusson <elmr20@student.bth.se>
2022-11-28 15:14:31 +0100

    update DB_Controller

--- api/DB_Controller.py
+++ api/DB_Controller.py
@@ -1,12 +1,31 @@
+from datetime import date
+import psycopg2
+
 class DBController:
     def __init__(self):
-        pass
-    def storeData():            #Store user scan
-        pass
-    def getData(UserID):        #get scans ? (report)
-        pass
-    def getUserRights(UserID):     #getUserData
-        pass
+        try:
+            self.conn = psycopg2.connect(
+                host = 'localhost',
+                dbname = 'postgres',
+                user = 'postgres',
+                password = 'url_scanner1',
+                port = 5432
+            )
+            self.cur = self.conn.cursor()
+        except (Exception, psycopg2.Error) as error:
+            print("Error occured while connecting to database", error)
+
+
+    def getUserRights(self, user_id):      
+        query = '''select rights_name from users inner join rights on users.rights_id = rights.rights_id and users.user_id = %s'''
+        query_values = (user_id)
+        self.cur.execute(query, query_values)
+        self.conn.commit()
+        return self.cur.fetchone()
+    
     def __del__(self):
-        if self.connection != None:
-                self.connection.close()
+        if self.cur is not None:
+            self.cur.close()
+        if self.conn is not None:
+            self.conn.close()
+    
