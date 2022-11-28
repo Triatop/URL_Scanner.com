2d23275d76030a152494a0885848db5d37e78624
Elliot Marcusson <elmr20@student.bth.se>
2022-11-28 23:34:55 +0100

    update DBController

--- api/DB_Controller.py
+++ api/DB_Controller.py
@@ -24,6 +24,12 @@ class DBController:
     def authenticateUser(self, username, user_token):
         pass
 
+    def getUserToken(self):
+        pass
+
+    def getHistory(self):
+        pass
+
     def authenticateAdmin(self, username):  
         query = '''select rights_name from users inner join rights on users.rights_id = rights.rights_id and users.username = '{}' '''.format(username)
         self.cur.execute(query)
@@ -31,14 +37,17 @@ class DBController:
         return self.format(self.cur.fetchone()) == 'admin'
 
     def checkUsernameExists(self, username):
-        query = '''select username from users where username='{}' '''.format(username)
-        self.cur.execute(query)
-        self.conn.commit()
-        return self.format(self.cur.fetchone()) == username
-    
+        try:        
+            query = '''select username from users where username='{}' '''.format(username)
+            self.cur.execute(query)
+            self.conn.commit()
+            return self.format(self.cur.fetchone()) == username
+        except (Exception, psycopg2.Error) as error:
+            print("Error occured while checking existing usernames", error)
+
     def createUser(self, username, en_pw, salt, fname, lname):
         rg_date = date.today()
-        try:
+        try:    
             query = '''insert into users(rights_id, username, en_pw, salt, fname, lname, rg_date) values('{}', '{}', '{}', '{}', '{}', '{}', '{}')'''.format(self.user_value, username, en_pw, salt, fname, lname, rg_date)
             self.cur.execute(query)
             self.conn.commit()      
