7c4ea0e33f47974a8162bae875584031e1fe110f
Elliot Marcusson <elmr20@student.bth.se>
2022-11-30 04:22:18 +0100

    update DB_controller

--- api/DB_Controller.py
+++ api/DB_Controller.py
@@ -1,6 +1,7 @@
 from datetime import date
 import psycopg2
 import re
+import hashlib, uuid
 
 class DBController:
     def __init__(self):
@@ -16,19 +17,33 @@ class DBController:
             self.user_value = 2011
         except (Exception, psycopg2.Error) as error:
             print("Error occured while connecting to database", error)
+    
+    def check_password(self, password, salt, en_pw):
+        return hashlib.sha512(password.encode('utf-8') + salt.encode('utf-8')).hexdigest() == en_pw
 
-    def format(self, string): 
-        return re.sub('[^A-Za-z0-9]+', '', str(string))
+    def hash_password(self, password):
+        salt = uuid.uuid4().hex
+        hashed_password = hashlib.sha512(password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+        return salt, hashed_password
+        
 
     def loginUser(self, username, password):
-        pass
+        try:
+            query = '''select en_pw, salt from users where username = '{}' '''.format(username)
+            self.cur.execute(query)
+            self.conn.commit()
+            fetch = self.cur.fetchall()[0]
+            en_pw = fetch[0]
+            salt = fetch[1]
+            return self.check_password(password, salt, en_pw)
+        except (Exception, psycopg2.Error) as error:
+            print("Error occured while logging in user", error)
 
-    
-    def authenticateUser(self, username, user_token):
-        pass
-
-    def getUserToken(self):
-        pass
+    def getUserToken(self, username):
+            query = '''select access_token from users where username = '{}' '''.format(username)
+            self.cur.execute(query)
+            self.conn.commit()
+            return self.cur.fetchone()[0]    
 
     def getHistory(self):
         pass
@@ -38,7 +53,7 @@ class DBController:
             query = '''select rights_name from users inner join rights on users.rights_id = rights.rights_id and users.username = '{}' '''.format(username)
             self.cur.execute(query)
             self.conn.commit()
-            return self.format(self.cur.fetchone()) == 'admin'
+            return self.cur.fetchone() == 'admin'
         except (Exception, psycopg2.Error) as error:
             print("Error occured while authenticating admin", error)
 
@@ -47,14 +62,14 @@ class DBController:
             query = '''select username from users where username='{}' '''.format(username)
             self.cur.execute(query)
             self.conn.commit()
-            return self.format(self.cur.fetchone()) == username
+            return self.cur.fetchone() == username
         except (Exception, psycopg2.Error) as error:
             print("Error occured while checking existing usernames", error)
 
-    def createUser(self, username, en_pw, salt, fname, lname):
+    def createUser(self, username, en_pw, salt, fname, lname, a_token):
         rg_date = date.today()
         try:    
-            query = '''insert into users(rights_id, username, en_pw, salt, fname, lname, rg_date) values('{}', '{}', '{}', '{}', '{}', '{}', '{}')'''.format(self.user_value, username, en_pw, salt, fname, lname, rg_date)
+            query = '''insert into users(rights_id, username, en_pw, salt, fname, lname, rg_date, access_token) values('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')'''.format(self.user_value, username, en_pw, salt, fname, lname, rg_date, a_token)
             self.cur.execute(query)
             self.conn.commit()      
         except (Exception, psycopg2.Error) as error:
@@ -114,19 +129,19 @@ class DBController:
 
         query4 = '''INSERT INTO rights
                 (rights_id, rights_name)
-                VALUES ( 1, 'admin' )'''
+                VALUES ( 2007, 'admin' )'''
         self.cur.execute(query4)
         self.conn.commit()
 
         query5 = '''INSERT INTO rights
                 (rights_id, rights_name)
-                VALUES ( 2, 'regUser' )'''
+                VALUES ( 2011, 'regUser' )'''
         self.cur.execute(query5)
         self.conn.commit()
 
         values = ('$2a$10$ovfJgA/SxVxsd3NeD3dMneuwYdimPJRdG.77eypo1KTlsgd7YmIYi', '$2a$10$YLXY3KPH3Kr0utTzmo6Eke4rAp8oZW0AQPuhaMkgqvi9GTcuo7Sc6')
         query6 = '''INSERT INTO users
                     ( rights_id, username, en_pw, salt, fname, lname, rg_date, access_token)
-                    VALUES ( 1, 'admin', %s, 'salt', 'admin', '', CURRENT_DATE, %s)'''
+                    VALUES ( 2007, 'admin', %s, 'salt', 'admin', '', CURRENT_DATE, %s)'''
         self.cur.execute(query6, values)
         self.conn.commit()
