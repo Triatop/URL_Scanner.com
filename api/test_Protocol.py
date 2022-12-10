afa5fc105eaa0027acb4b7f069b5e2182eaff66e
Triatop <kimdenlille@gmail.com>
2022-12-10 11:28:14 +0000

    Revising

--- api/test_Protocol.py
+++ api/test_Protocol.py
@@ -2,36 +2,31 @@ import Protocol_URL as PURL
 
 def test_getData():
     Pro = PURL.Protocol_URL()
-    Pro2 = PURL.Protocol_URL()
-    Pro3 = PURL.Protocol_URL()
-    p_rätt = 'https'
-    p_fel = 'http'
-    p_fler = ['https','http']
-    assert(Pro.getData(p_rätt))
-    assert(Pro.currentProtocol == p_rätt)
-    assert(Pro.secureProtocols[0] == p_rätt)
-    assert(Pro2.getData(p_fler))
-    assert(Pro2.currentProtocol == p_fler[0])
-    assert(Pro2.secureProtocols == p_fler)
-    assert(Pro3.getData(p_fel))
-    assert(Pro3.currentProtocol == p_fel)
-    assert(Pro3.secureProtocols[0] == p_fel)
+    p_correct = 'https'
+    p_incorrect = 'http'
+    p_more = ['https','http']
+    assert(Pro.getData(p_correct))
+    assert(Pro.currentProtocol == p_correct)
+
+    assert(Pro.getData(p_more))
+    assert(Pro.currentProtocol == p_more[0])
+    
+    assert(Pro.getData(p_incorrect))
+    assert(Pro.currentProtocol == p_incorrect)
     return
 
 def test_isSecure():
     Pro = PURL.Protocol_URL()
-    Pro2 = PURL.Protocol_URL()
-    Pro3 = PURL.Protocol_URL()
-    p_rätt = 'https'
-    p_fel = 'http'
-    p_fler = ['https','http']
+    p_correct = 'https'
+    p_incorrect = 'http'
+    p_more = ['https','http']
 
-    Pro.getData(p_rätt)
-    Pro2.getData(p_fler)
-    Pro3.getData(p_fel)
 
+    Pro.getData(p_correct)
+    assert(Pro.isSecure() == 1)
+    Pro.getData(p_more)
+    assert(Pro.isSecure() == 1)
+    Pro.getData(p_incorrect)
     assert(Pro.isSecure() == 0)
-    assert(Pro2.isSecure() == 0)
-    assert(Pro3.isSecure() == 1)
 
     return
