5597fae648fe3cb26ac68b5f6863c881c2fd4308
Triatop <kimdenlille@gmail.com>
2022-12-14 10:48:48 +0000

    Update CharSwap.py

--- api/CharSwap.py
+++ api/CharSwap.py
@@ -54,7 +54,7 @@ class CharSwap:
     def stripAccents(self): #take a character "ö" that has accents (the dots) and transform it "o" to its lookalike character
             self.sus_url = unidecode(self.sus_url)
             return 1
-       
+
     def isTop500(self):
         found = 0
         try:
@@ -64,7 +64,7 @@ class CharSwap:
                     if self.sus_url == line[0]:                 #if it exists in list
                         found = 1                               #the website tried to trick us
                         break                                   #no need to check the others
-        except EnvironmentError as err:
+        except Exception as err:
             logging.warning(f"CharSwap.isTop500() - {err}")
         return found
 
