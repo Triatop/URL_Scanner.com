762bf5b5079a441a31c5f2ef3cdb29382beda633
Narfis <38036515+Narfis@users.noreply.github.com>
2022-12-14 11:44:12 +0100

    everything is now within fucntions

--- api/dataMaker.py
+++ api/dataMaker.py
@@ -1,16 +1,10 @@
 import main
 import csv
 import json
-import func_timeout
 import threading
 import os
 import time
 import pandas as pd
-theVals = []
-
-
-data = ["Site","FavIcon","SecureProtocol","CheckPort","SiteAge","SiteLength","SpecialCharacters","Certificate","Malicious Links","Swapped","Pulse Count","Malicious count","IsSafe"]
-
 
 def extention(file):
     return os.path.splitext(file)[-1].lower()
@@ -22,23 +16,21 @@ def loadData(file, ext):
     if ext == '.json':
         with open(file, 'r') as f:
             return json.load(f)
-theFile = 'bad.csv'
-ext = extention(theFile)
-jsonData = loadData(theFile, ext) 
-if ext == '.json':
-    keysList = list(jsonData)
-    valueList = list(jsonData.values())
-if ext == '.csv':
-    keysList = list(jsonData['Site'])
-    valueList = list(jsonData['IsSafe'])
 
+def getValues(data, ext):
+    if ext == '.json':
+        keysList = list(data)
+        valueList = list(data.values()) 
+    if ext == '.csv':
+        keysList = list(data['Site'])
+        valueList = list(data['IsSafe'])
+    return keysList, valueList
 
-def processData(start, steps):
+def processData(start, steps, keysList, valueList, theVals):
         for i in range(start, start+steps):
             try:
                 time.sleep(2)
                 val = {'Site' : keysList[i]}
-                # val.update(func_timeout.func_timeout(30, main.main, (keysList[i],'admin', False)))
                 val.update(main.main(keysList[i],'admin', False))
                 val['IsSafe'] = (0 if valueList[i] == False else 1)
                 if('valid' not in val):
@@ -46,35 +38,46 @@ def processData(start, steps):
                     theVals.append(val.values())
             except:
                 print(keysList[i], "Not working")
-                pass
+                continue
 
 
 
-def makeWorkers():
+def makeWorkers(data):
     workers = 1
-    while len(jsonData)%workers != 0:
+    while len(data)%workers != 0:
         workers -= 1
     return workers
 
-def startThreads():
-    workers = makeWorkers()
+def startThreads(data, vals, theAttributes):
+
+    workers = makeWorkers(data)
     threads = []
-    #length = int(len(jsonData)/workers)
-    length = 20
-    currVal = 7
-    #CurrVal = 0, bord läggas till 1 när denna är klar
+    length = 5
+    currVal = 0
     for i in range(workers):
-        threads.append(threading.Thread(target=processData, args=(currVal*length,length)))
+        threads.append(threading.Thread(target=processData, args=(currVal*length,length, vals[0], vals[1], theAttributes)))
     for thread in threads:
         thread.start()
     for thread in threads:
         thread.join()
 
-startThreads()
+
+def mainFunc():
+    firstLine = ["Site","FavIcon","SecureProtocol","CheckPort","SiteAge","SiteLength","SpecialCharacters","Certificate","Malicious Links","Swapped","Pulse Count","Malicious count","IsSafe"]
+
+    theFile = 'example.csv'
+    ext = extention(theFile)
+    data = loadData(theFile, ext)
+    values = getValues(data, ext)
+    theAttributes = []
 
 
-with open('fixedGood.csv', 'w') as file:
-    writer = csv.writer(file)
-    writer.writerow(data)
-    writer.writerows(theVals)
+    startThreads(data, values, theAttributes)
 
+    with open('yes.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(firstLine)
+        writer.writerows(theAttributes)
+
+if __name__ == '__main__':
+    mainFunc()
