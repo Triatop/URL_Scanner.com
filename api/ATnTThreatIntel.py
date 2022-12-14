288ab6abbef6c33e7081ce378403e83a5857754e
Triatop <kimdenlille@gmail.com>
2022-12-14 10:11:42 +0000

    Extended error handing and logging

--- api/ATnTThreatIntel.py
+++ api/ATnTThreatIntel.py
@@ -15,19 +15,41 @@ class ATnTThreatIntel:
         if self.__checkValidInput(url, method = ".maliciousFiles()"):
             return None
         url = self.__getDomain(url)
-        passive_dns = requests.get(f"https://otx.alienvault.com/api/v1/indicators/hostname/{url}/passive_dns").json()["passive_dns"]
+        try:
+            passive_dns = requests.get(f"https://otx.alienvault.com/api/v1/indicators/hostname/{url}/passive_dns").json()["passive_dns"]
+        except EnvironmentError as err:
+            logging.warning(f"ATnTThreatIntel.maliciousFiles({add}) - {err}")
         addresses = []
         filesCount = {}
-        filesCount[url] = requests.get("https://otx.alienvault.com/api/v1/indicators/hostname/{}/malware".format(url)).json()["count"]
+        try:
+            filesCount[url] = requests.get("https://otx.alienvault.com/api/v1/indicators/hostname/{}/malware".format(url)).json()["count"]
+        except EnvironmentError as err:
+            logging.warning(f"ATnTThreatIntel.maliciousFiles({add}) - {err}")
         for add in passive_dns:
             addresses.append(add["address"])
         for add in addresses:
             try:
-                if re.match("[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", add):
-                    filesCount[add] = requests.get("https://otx.alienvault.com/api/v1/indicators/IPv4/{}/malware".format(add)).json()["count"]
+                if re.match("^ns|^NXDOMAIN", add):
+                    continue
+                if re.match(r"(\d{1,3}\.){3}\d{1,3}", add):
+                    try:
+                        filesCount[add] = requests.get("https://otx.alienvault.com/api/v1/indicators/IPv4/{}/malware".format(add)).json()["count"]
+                    except:
+                        logging.warning(f"Couldn't process {add} in .getMaliciousFilesCount() /IPv4/../malware api call")
+                elif re.match("^((\d|\w){0,4}:){1,7}(\d|\w){0,4}", add):
+                    try:
+                        filesCount[add] = requests.get("https://otx.alienvault.com/api/v1/indicators/IPv6/{}/malware".format(add)).json()["count"]
+                    except:
+                        logging.warning(f"Couldn't process {add} in .getMaliciousFilesCount() /IPv6/../malware api call")
                 else:
-                    filesCount[add] = requests.get("https://otx.alienvault.com/api/v1/indicators/hostname/{}/malware".format(add)).json()["count"]
+                    try:
+                        filesCount[add] = requests.get("https://otx.alienvault.com/api/v1/indicators/hostname/{}/malware".format(add)).json()["count"]
+                    except:
+                        logging.warning(f"Couldn't process {add} in .getMaliciousFilesCount() /hostname/../malware api call")
             except KeyError:
+                continue
+            except EnvironmentError as err:
+                logging.warning(f"ATnTThreatIntel.maliciousFiles({add}) - {err}")
                 continue
         return filesCount
     def getMaliciousFilesCount(self, url = "", limit = 100):
@@ -37,9 +59,15 @@ class ATnTThreatIntel:
         if self.__checkValidInput(url, method = ".getMaliciousFilesCount()"):
             return None
         url = self.__getDomain(url)
-        passive_dns = requests.get(f"https://otx.alienvault.com/api/v1/indicators/hostname/{url}/passive_dns").json()["passive_dns"]
+        try:
+            passive_dns = requests.get(f"https://otx.alienvault.com/api/v1/indicators/hostname/{url}/passive_dns").json()["passive_dns"]
+        except EnvironmentError as err:
+            logging.warning(f"ATnTThreatIntel.maliciousFilesCount({add}, {limit}) - {err}")
         addresses = []
-        filesCount = requests.get("https://otx.alienvault.com/api/v1/indicators/hostname/{}/malware".format(url)).json()["count"]
+        try:
+            filesCount = requests.get("https://otx.alienvault.com/api/v1/indicators/hostname/{}/malware".format(url)).json()["count"]
+        except EnvironmentError as err:
+            logging.warning(f"ATnTThreatIntel.maliciousFilesCount({add}, {limit}) - {err}")
         for add in passive_dns:
             addresses.append(add["address"])
         addresses = [*set(addresses)]   # Clear duplicate addresses
@@ -67,6 +95,9 @@ class ATnTThreatIntel:
                     break
             except KeyError:
                 continue
+            except EnvironmentError as err:
+                logging.warning(f"ATnTThreatIntel.getMaliciousFilesCount({add}, {limit}) - {err}")
+                continue
         return filesCount
 
 
@@ -90,11 +121,14 @@ class ATnTThreatIntel:
         if self.__checkValidInput(url, method=".pulseCount()"):
             return None
         url = self.__getDomain(url)
-        req = requests.get(f"https://otx.alienvault.com/api/v1/indicators/domain/{url}/general").json()
-        try: 
+        try:
+            req = requests.get(f"https://otx.alienvault.com/api/v1/indicators/domain/{url}/general").json()
             return req["pulse_info"]["count"]
         except KeyError:
-            return None
+            pass
+        except EnvironmentError as err:
+            logging.warning(f"ATnTThreatIntel.puleCount({url}) - {err}")
+        return None
         
 
     def __checkValidInput(self, url, method):
